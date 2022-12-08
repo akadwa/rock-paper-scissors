59542cf9c650eb0cad0ace63d421dbf86b6e9a3c
akadwa <kadazh123@gmail.com>
2022-12-08 20:40:05 +0200

    Create playRound function

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ function getComputerChoice() {
     return(randomComputerSelection);
 }
 
+// Function to play a single round
+function playRound(playerSelection, computerSelection) {
+    // draw condition in case of both the player and computer selecting the same option
+    if (playerSelection == computerSelection) {
+        return "It's a draw!";
+    }
+    // player win conditions:
+    else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return "You win! Rock beats scissors!";
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'rock'){
+        return "You win! Paper beats rock!";
+    }
+    else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return "You win! Scissors beats paper!";
+    }
+    // player lose conditions:
+    else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return "You lose! Rock beats scissors!";
+    }
+    else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return "You lose! Paper beats rock!";
+    }
+    else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return "You lose! Scissors beats paper!";
+    }
+}
